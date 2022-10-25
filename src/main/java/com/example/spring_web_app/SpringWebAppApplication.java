a8a651ef9a2e76d1e307ed9046236af690cd507d
annasentiabova <s.anuna1401@gmail.com>
2022-10-25 10:06:35 +0500

    lab 5 add all

---
+++ src/main/java/com/example/spring_web_app/SpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebAppApplication.class, args);
+    }
+
+}
